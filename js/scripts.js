5f7cb61f4dc9ee5fa310cf454f949aa4da2dcceb
oinktech <oinktech2024@gmail.com>
2024-09-15 20:49:38 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
-// 动态加载脚本
-const script = document.createElement('script');
-script.src = 'https://oinktech.github.io/website-db-manger/script.js';
-script.defer = true;
-document.head.appendChild(script);
+const script = document.createElement('script'); // 创建一个新的 <script> 元素
+    script.src = 'https://oinktech.github.io/website-db-manger/script.js'; // 设置脚本的 URL
+    document.head.appendChild(script); // 将脚本元素添加到 <head> 部分
 
 // 确保脚本加载完成后再执行
 script.onload = () => {

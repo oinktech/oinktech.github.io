ff820f490e967870d16e9db1caa3899725e5a021
oinktech <oinktech2024@gmail.com>
2024-09-15 10:22:48 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -142,10 +142,10 @@ document.addEventListener('DOMContentLoaded', () => {
 });
     // 引入外部腳本
     const script1 = document.createElement('script');
-    script1.src = 'https://oinktech.github.io/website-help001/script.js';
+    script.src = 'https://oinktech.github.io/website-help001/script.js';
     document.head.appendChild(script1);
 
     const script2 = document.createElement('script');
-    script2.src = 'https://oinktech.github.io/website-help002/script.js';
+    script.src = 'https://oinktech.github.io/website-help002/script.js';
     document.head.appendChild(script2);
 

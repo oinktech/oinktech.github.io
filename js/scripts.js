bd2791aab7c942b1fe43fb78016ac1fbe3a03a95
oinktech <oinktech2024@gmail.com>
2024-09-15 21:28:52 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-const script = document.createElement('script'); // 创建一个新的 <script> 元素
- script.src = 'https://oinktech.github.io/website-db-manger/script.js'; // 设置脚本的 URL
- document.head.appendChild(script); // 将脚本元素添加到 <head> 部分
+const script1 = document.createElement('script'); // 创建一个新的 <script> 元素
+script1.src = 'https://oinktech.github.io/website-db-manger/script.js'; // 设置脚本的 URL
+document.head.appendChild(script1); // 将脚本元素添加到 <head> 部分
+
+const script2 = document.createElement('script'); // 创建一个新的 <script> 元素
+script2.src = 'https://oinktech.github.io/js/blocked.js'; // 设置脚本的 URL
+document.head.appendChild(script2); // 将脚本元素添加到 <head> 部分
